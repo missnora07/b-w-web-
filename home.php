82feb0cfc472f842c794dbdc9b366bf5ff033670
missnora07 <107804394+missnora07@users.noreply.github.com>
2022-09-09 12:06:19 +0530

    B&w web
    Test

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		  <link rel="stylesheet" href="styles.css">
+		</head>
+<body>
+<center><h1>HOME<h1>
+<h2>DARK WEB</h2>
+<em>
+	<p>Hello.. 
+<?php
+echo $_POST["n1"]
+?>,
+  <br>Welcome to Test_Web
+</em></center>
+</body>
+<center><p>This is a test project and welcome to my test project</p></center>
